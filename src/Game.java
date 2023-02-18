21380bc87e10585430cea45428d1042c24344dc6
asan6602 <110870409+asan6602@users.noreply.github.com>
2023-02-18 13:07:21 -0500

    batting print format

--- src/Game.java
+++ src/Game.java
@@ -17,15 +17,16 @@ public class Game {
         for(ArrayList<ArrayList<String>> entry: stats) {
             if(counter == 0) {
                 System.out.println("Name: ");
+                System.out.println(entry.get(0));
             }
             if(counter == 1) {
                 System.out.println("Batting: ");
+                System.out.println("[Year, Team,   G,  AB,   R,   H,  2B,  3B,  HR, RBI,  BB,  SB,   AVG]");
+                printHitting(entry);
             }
             if(counter == 2) {
                 System.out.println("Pitching: ");
-            }
-            for(ArrayList<String> entry2: entry) {
-                System.out.println(entry2);
+                System.out.println("[Year, Team,   G, W, L, SV, SO, ERA, Outs]");
             }
             counter++;
         }
@@ -60,6 +61,115 @@ public class Game {
 
         }
     }
+
+    public void printHitting(ArrayList<ArrayList<String>> entry) {
+        for(ArrayList<String> entry2: entry) {
+            int abs = 0;
+            int hits = 0;
+            System.out.print("[");
+                    int index= 0;
+                    for(String stat: entry2) {
+                        if(index == 0) {
+                            System.out.print(stat + ",");
+                        }
+                        if(index == 1) {
+                            System.out.print("  "+ stat + ",");
+                        }
+                        if(index == 2) {
+                            statlength(stat);
+                        }
+                        if(index == 3) {
+                            statlength(stat);
+                            abs = Integer.parseInt(stat);
+                        }
+                        if(index == 4) {
+                            statlength(stat);
+                        }
+                        if(index == 5) {
+                            statlength(stat);
+                            hits = Integer.parseInt(stat);
+                        }
+                        if(index == 6) {
+                            statlength(stat);
+                        }
+                        if(index == 7) {
+                            statlength(stat);
+                        }
+                        if(index == 8) {
+                            statlength(stat);
+                        }
+                        if(index == 9) {
+                            statlength(stat);
+                        }
+                        if(index == 10) {
+                            statlength(stat);
+                        }
+                        if(index == 11) {
+                            if(stat.length() == 1) {
+                                System.out.print("   " + stat + ", ");
+                            }
+                            if(stat.length() == 2) {
+                                System.out.print("  " + stat + ", ");
+                            }
+                            if(stat.length() == 3) {
+                                System.out.print(" " + stat + ", ");
+                            }
+                        }
+                        index += 1;
+                    }
+                    printAverage(hits, abs);
+                    System.out.println("]");
+        }
+    }
+
+    public void printAverage(int hits, int abs) {
+        float average;
+        if(hits == 0 || abs == 0) {
+            System.out.print("0.000");
+            return;
+        }
+        else {
+            average = (float)hits / abs;
+
+        }
+        String averageSTR = Float.toString(average);
+        char[] averageCHAR = averageSTR.toCharArray();
+
+        int counter = 0;
+        for(char c: averageCHAR) {
+            if (counter > 4) {
+                break;
+            }
+            else {
+                System.out.print(c);
+            }
+            counter++;
+        }
+        if(averageCHAR.length == 3) {
+            System.out.print("00");
+        }
+        if(averageCHAR.length == 4) {
+            System.out.print("0");
+        }
+        
+    }
+
+    public void printPitching(ArrayList<ArrayList<String>> entry) {
+
+    }
+
+    public void statlength(String stat) {
+        if(stat.length() == 1) {
+            System.out.print("   " + stat + ",");
+        }
+        if(stat.length() == 2) {
+            System.out.print("  " + stat + ",");
+        }
+        if(stat.length() == 3) {
+            System.out.print(" " + stat + ",");
+        }
+    }
+    
     public void changeSetting(Setting setting) {
         this.gameSetting = setting;
     }
@@ -68,26 +178,26 @@ public class Game {
      * checks if the player has the relevant amount of games played
      */
     private boolean relevancyCheck(String playerID) {
-        String sql1 = String.format("SELECT SUM(g) FROM batting WHERE batting.playerID ='%s'", playerID);
-        String sql2 = String.format("SELECT SUM(g) FROM pitching WHERE pitching.playerID ='%s'", playerID);
+        String sql1 = String.format("SELECT SUM(h) FROM batting WHERE batting.playerID ='%s'", playerID);
+        String sql2 = String.format("SELECT SUM(so) FROM pitching WHERE pitching.playerID ='%s'", playerID);
 
-        int battingGames = 0;
+        int battingH = 0;
         if (db_Utils.exec_get_all(sql1).get(0).get(0) != null) {
-            battingGames = Integer.parseInt(db_Utils.exec_get_all(sql1).get(0).get(0));
+            battingH = Integer.parseInt(db_Utils.exec_get_all(sql1).get(0).get(0));
         }
 
         
-        int pitchingGames = 0;
+        int pitchingSO = 0;
         if(db_Utils.exec_get_all(sql2).get(0).get(0) != null) {
-            pitchingGames = Integer.parseInt(db_Utils.exec_get_all(sql2).get(0).get(0));
+            pitchingSO = Integer.parseInt(db_Utils.exec_get_all(sql2).get(0).get(0));
         }
 
         boolean result = false;
 
-        if(battingGames > 999) {
+        if(battingH > 700) {
             result = true;
         }
-        if(pitchingGames > 200) {
+        if(pitchingSO > 800) {
             result = true;
         }
         return result;
